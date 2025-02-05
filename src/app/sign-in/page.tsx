fb8b72819002cc6b084232cea87bc9bd77f1b90d
racine14creator <bisimwagrace596@gmail.com>
2025-02-05 11:01:49 +0200

    UI Login implemented

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -11,7 +11,7 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import React from "react";
 
-export default function SignUpPage() {
+export default function SignInPage() {
   return (
     <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
       <Card className='max-w-sm mx-auto'>
@@ -20,24 +20,18 @@ export default function SignUpPage() {
             <h3 className='text-3xl font-bold text-red-500'>Sign Up</h3>
           </CardTitle>
           <CardDescription>
-            Use your Email, Full name, and Password to create your account for
-            free
+            Sign-In with your Email and Password to access your account
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form action='' className='gap-y-3'>
-            <div className='flex flex-col gap-2'>
-              <Label htmlFor='fullname'>Fullname</Label>
-              <Input id='fullname' type='text' placeholder='John Doe' />
-              <p className='text-red-500 text-thin text-sm'>error here...</p>
-            </div>
             <div className='flex flex-col gap-2'>
               <Label htmlFor='email'>Email Address</Label>
               <Input id='email' type='email' placeholder='email@example.next' />
               <p className='text-red-500 text-thin text-sm'>error here...</p>
             </div>
             <div className='flex flex-col gap-2'>
-              <Label htmlFor='Pasword'>Password</Label>
+              <Label htmlFor='password'>Password</Label>
               <Input id='password' type='password' placeholder='Password' />
               <p className='text-red-500 text-thin text-sm'>error here...</p>
             </div>
@@ -49,7 +43,7 @@ export default function SignUpPage() {
             <div className='mt-5'>
               <p className='font-light text-md text-left'>
                 Already have an{" "}
-                <Link href='/sign-in' className='text-blue-500 underline'>
+                <Link href='/sign-up' className='text-blue-500 underline'>
                   account!
                 </Link>
               </p>
